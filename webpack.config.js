20a64108264fb31693df74402b1fc47162dd5a65
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-03-29 01:42:46 +0300

    feat: add webpack plugins and npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -9,5 +11,11 @@ module.exports = {
     output: {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
