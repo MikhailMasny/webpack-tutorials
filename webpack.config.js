000e9b08c294842c1ec8db591132c9019c457fe1
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-03-29 03:31:20 +0300

    feat: add expanded webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
@@ -30,7 +31,13 @@ module.exports = {
         new HTMLWebpackPlugin({
             template: "./index.html"
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, "src/favicon.ico"),
+                to: path.resolve(__dirname, "dist")
+            }
+        ])
     ],
     module: {
         rules: [
